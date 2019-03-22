ea7542e326330c7534365b957e57708cd0ba02f1
cado19 <mryoungrichie@gmail.com>
2019-03-22 23:34:46 +0300

    Added turbo links load so that the search field can work without requiring page reload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require rails-ujs
 //= require jquery
+//= require jquery-ujs
 //= require turbolinks
 //= require popper
 //= require bootstrap
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on("turbolinks:load", function(){
   $('.searchField').on('keyup', function(){
     var value = $(this).val().toLowerCase();
     $('#myTable tr').filter(function(){
